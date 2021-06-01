4a00aaa3c1a920f869cdfeaa057fb17bbdd983e9
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-06-01 22:26:52 +0300

    product list

--- Development/react/electronic/src/App.js
+++ Development/react/electronic/src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
+import products from "./products";
 
 function App() {
+  const productsList = products.map((product) => (
+    <div>
+      <div className="product-image">
+        <img src={product.image} alt="mobile" />
+      </div>
+      <p className="img-text">{product.name}</p>
+      <p className="img-text">{product.price + " KD"}</p>
+    </div>
+  ));
   return (
-    <div className="App App-header">
-      <h1>electronic store</h1>
-      <p>
-        this the best electronic store that you can find the newest product in a
-        appropriate price{" "}
-      </p>
-      <img
-        className=""
-        src="https://www.midvalley.com.my/img/tenant/Flash%20Gadget%20Mobile-425Jun2020155009.jpg"
-        alt="electronic store"
-      />
+    <div>
+      <div>
+        <div className="text-style">
+          <h1>electronic store</h1>
+          <p middle-text>
+            this the best electronic store that you can find the newest product
+            in a appropriate price{" "}
+          </p>
+        </div>
+        <img
+          className="shop-image"
+          src="https://www.midvalley.com.my/img/tenant/Flash%20Gadget%20Mobile-425Jun2020155009.jpg"
+          alt="electronic store"
+        />
+      </div>
+      <div className="products-list">{productsList}</div>
     </div>
   );
 }
---
+++ Development/react/electronic/src/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    name: "iphone7",
+    price: 300,
+    image:
+      "https://cdn.alloallo.media/catalog/product/apple/iphone/iphone-7/iphone-7-black.jpg",
+  },
+  {
+    name: "samsung note20",
+    price: 500,
+    image:
+      "https://images.samsung.com/is/image/samsung/assets/levant/smartphones/buy/shop/001-note20series-productimage-mo-720.jpg",
+  },
+  {
+    name: "oppo",
+    price: 300,
+    image:
+      "https://www.oppo.com/content/dam/oppo/common/mkt/v2-2/a15/navigation/A15-navigation-blue-v2.png",
+  },
+];
+export default products;
