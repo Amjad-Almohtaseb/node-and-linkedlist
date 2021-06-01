4e08bc444fdbb6f3c2d87d2d8ed33e312c33bd20
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-06-01 14:16:05 +0300

    electronic

---
+++ Development/react/electronic/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App App-header">
+      <h1>electronic store</h1>
+      <p>
+        this the best electronic store that you can find the newest product in a
+        appropriate price{" "}
+      </p>
+      <img
+        className=""
+        src="https://www.midvalley.com.my/img/tenant/Flash%20Gadget%20Mobile-425Jun2020155009.jpg"
+        alt="electronic store"
+      />
+    </div>
+  );
+}
+
+export default App;
